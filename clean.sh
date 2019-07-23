a1abeefb8fb911f72d63c8feb628b3c761e57817
deng.liu <deng.liu@amlogic.com>
2019-07-23 15:09:26 +0800

    npu: deltete extra files [1/1]
    Change-Id: Ifd0119d220765c358272572ff7be40142ca4c537
    Signed-off-by: deng.liu <deng.liu@amlogic.com>

--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find -name *.o|xargs rm -rf
-find -name *.d|xargs rm -rf
-find -name *.cmd|xargs rm -rf
--- export.sh
+++
@@ -1,3 +0,0 @@
-export AQROOT=/mnt/fileroot/xingwei.zhou/work/pxp_bringup/pxp_driver/baseDriver
-export AQARCH=$AQROOT/arch/XAQ2
-echo $AQARCH
